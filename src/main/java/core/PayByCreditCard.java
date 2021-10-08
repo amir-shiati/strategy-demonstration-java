61d6a679f7d5860893632e3c2d6a908fac74545d
amir shiati <amirshiati98@gmail.com>
2021-10-09 01:03:18 +0330

    finished added strategy pattern functinalities

--- src/main/java/core/PayByCreditCard.java
+++ src/main/java/core/PayByCreditCard.java
@@ -1,8 +1,23 @@
 package core;
 
-public class PayByCreditCard implements PayStrategy{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PayByCreditCard implements PayStrategy {
+    private final BufferedReader Reader = new BufferedReader(new InputStreamReader(System.in));
+
     @Override
     public void pay(int amount) {
-
+        try {
+            System.out.println("Enter your card number: ");
+            String cardNumber = Reader.readLine();
+            System.out.println("Enter the CVV code: ");
+            String cvv = Reader.readLine();
+            // Process the payment using the credit card
+            System.out.println("The payment was successful." + " The total was: " + amount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/core/PayByEmail.java
+++ src/main/java/core/PayByEmail.java
@@ -1,8 +1,23 @@
 package core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PayByEmail implements PayStrategy {
+    private final BufferedReader Reader = new BufferedReader(new InputStreamReader(System.in));
+
     @Override
     public void pay(int amount) {
-
+        try {
+            System.out.println("Enter your email: ");
+            String email = Reader.readLine();
+            System.out.println("Enter your password: ");
+            String password = Reader.readLine();
+            // Process the payment using the email provided
+            System.out.println("The payment was successful." + " The total was: " + amount);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
